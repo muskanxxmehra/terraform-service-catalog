707010db419a59b9ae7ab10c53edeb7fe7a9737b
muskanxxmehra <muskan.mehra@azalio.io>
2026-01-15 02:15:24 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,11 +1,38 @@
+################################################################################
+# VPC Module - Outputs
+################################################################################
+
 output "vpc_id" {
-  value = aws_vpc.this.id
+  description = "The ID of the VPC"
+  value       = aws_vpc.this.id
+}
+
+output "vpc_cidr" {
+  description = "The CIDR block of the VPC"
+  value       = aws_vpc.this.cidr_block
 }
 
 output "public_subnet_id" {
-  value = aws_subnet.public.id
+  description = "The ID of the public subnet"
+  value       = aws_subnet.public.id
 }
 
 output "private_subnet_id" {
-  value = aws_subnet.private.id
+  description = "The ID of the private subnet"
+  value       = aws_subnet.private.id
+}
+
+output "internet_gateway_id" {
+  description = "The ID of the Internet Gateway"
+  value       = aws_internet_gateway.this.id
+}
+
+output "public_route_table_id" {
+  description = "The ID of the public route table"
+  value       = aws_route_table.public.id
+}
+
+output "private_route_table_id" {
+  description = "The ID of the private route table"
+  value       = aws_route_table.private.id
 }
