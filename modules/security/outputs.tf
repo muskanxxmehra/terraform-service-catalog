dbfe82b1429ee0b3d9f70cf461f2c45e54111121
muskanxxmehra <muskan.mehra@azalio.io>
2026-01-15 02:14:03 +0530

    Update outputs.tf

--- modules/security/outputs.tf
+++ modules/security/outputs.tf
@@ -1,8 +1,23 @@
+################################################################################
+# Security Module - Outputs
+################################################################################
+
 output "app_sg_id" {
-  value = aws_security_group.app_sg.id
+  description = "The ID of the application security group"
+  value       = aws_security_group.app.id
+}
+
+output "app_sg_name" {
+  description = "The name of the application security group"
+  value       = aws_security_group.app.name
 }
 
 output "db_sg_id" {
-  value = aws_security_group.db_sg.id
+  description = "The ID of the database security group"
+  value       = aws_security_group.db.id
 }
 
+output "db_sg_name" {
+  description = "The name of the database security group"
+  value       = aws_security_group.db.name
+}
