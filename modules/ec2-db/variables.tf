a2e4543b32786b945a39e1c853b64b63381edc46
muskanxxmehra <muskan.mehra@azalio.io>
2026-01-15 02:12:57 +0530

    Update variables.tf

--- modules/ec2-db/variables.tf
+++ modules/ec2-db/variables.tf
@@ -1,5 +1,73 @@
-variable "ami_id" {}
-variable "instance_type" {}
-variable "subnet_id" {}
-variable "security_group_id" {}
+################################################################################
+# EC2 DB Module - Variables
+################################################################################
 
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type (t2.micro for Free Tier)"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where the instance will be launched (should be private)"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "Security group ID to attach to the instance"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair"
+  type        = string
+  default     = null
+}
+
+variable "environment" {
+  description = "Environment name for tagging"
+  type        = string
+  default     = "dev"
+}
+
+variable "root_volume_size" {
+  description = "Size of the root EBS volume in GB (Free Tier: up to 30GB)"
+  type        = number
+  default     = 8
+}
+
+variable "db_name" {
+  description = "Name of the database to create"
+  type        = string
+  default     = "appdb"
+}
+
+variable "db_user" {
+  description = "Database user to create"
+  type        = string
+  default     = "appuser"
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true
+  default     = "ChangeMe123!"
+}
+
+variable "user_data" {
+  description = "User data script for instance initialization"
+  type        = string
+  default     = ""
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default     = {}
+}
