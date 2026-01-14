cd2fdfa4c64d3fb2c44dde5283056a6976731557
muskanxxmehra <muskan.mehra@azalio.io>
2026-01-15 01:22:59 +0530

    Update main.tf
    update main file in vpc

---
+++ modules/vpc/main.tf
@@ -0,0 +1,34 @@
+resource "aws_vpc" "this" {
+  cidr_block = var.vpc_cidr
+  tags       = var.tags
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.this.id
+  cidr_block = var.private_subnet_cidr
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_route" "internet" {
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.this.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
