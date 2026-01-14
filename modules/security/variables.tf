1e66c7568fa6cbf99a34b4d3493143a452de19a7
muskanxxmehra <muskan.mehra@azalio.io>
2026-01-15 02:14:33 +0530

    Update variables.tf

--- modules/security/variables.tf
+++ modules/security/variables.tf
@@ -1,4 +1,32 @@
+################################################################################
+# Security Module - Variables
+################################################################################
+
 variable "vpc_id" {
   description = "VPC ID where security groups will be created"
   type        = string
+}
+
+variable "environment" {
+  description = "Environment name for tagging"
+  type        = string
+  default     = "dev"
+}
+
+variable "ssh_allowed_cidr" {
+  description = "CIDR blocks allowed for SSH access to app server"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+variable "app_port" {
+  description = "Custom application port to open (optional)"
+  type        = number
+  default     = null
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default     = {}
 }
