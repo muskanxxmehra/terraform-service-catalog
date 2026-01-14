cbe49cde7988a78dc624a8229d7c9cd9510539bd
muskanxxmehra <muskan.mehra@azalio.io>
2026-01-15 02:12:33 +0530

    Update outputs.tf

--- modules/ec2-db/outputs.tf
+++ modules/ec2-db/outputs.tf
@@ -1,4 +1,28 @@
-output "private_ip" {
-  value = aws_instance.this.private_ip
+################################################################################
+# EC2 DB Module - Outputs
+################################################################################
+
+output "instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.db.id
 }
 
+output "instance_arn" {
+  description = "The ARN of the EC2 instance"
+  value       = aws_instance.db.arn
+}
+
+output "private_ip" {
+  description = "The private IP address of the instance"
+  value       = aws_instance.db.private_ip
+}
+
+output "private_dns" {
+  description = "The private DNS name of the instance"
+  value       = aws_instance.db.private_dns
+}
+
+output "db_endpoint" {
+  description = "Database connection endpoint"
+  value       = "${aws_instance.db.private_ip}:3306"
+}
