e441147f8f1f1f2fd1da5abf8f12ea247de9118c
muskanxxmehra <muskan.mehra@azalio.io>
2026-01-15 02:15:56 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,19 +1,39 @@
+################################################################################
+# VPC Module - Variables
+################################################################################
+
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidr" {
-  description = "CIDR block for public subnet"
+  description = "CIDR block for the public subnet"
   type        = string
+  default     = "10.0.1.0/24"
 }
 
 variable "private_subnet_cidr" {
-  description = "CIDR block for private subnet"
+  description = "CIDR block for the private subnet"
   type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "availability_zone" {
+  description = "Availability zone for subnets"
+  type        = string
+  default     = "us-east-1a"
+}
+
+variable "environment" {
+  description = "Environment name for tagging"
+  type        = string
+  default     = "dev"
 }
 
 variable "tags" {
-  description = "Tags applied to VPC resources"
+  description = "Tags to apply to all resources"
   type        = map(string)
+  default     = {}
 }
