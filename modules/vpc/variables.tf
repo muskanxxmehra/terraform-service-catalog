e8c5c597e902f5dc8ac817ef16f78cb10a22cef7
muskanxxmehra <muskan.mehra@azalio.io>
2026-01-15 01:21:25 +0530

    Update variables.tf
    updated vpc variables

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for public subnet"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for private subnet"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags applied to VPC resources"
+  type        = map(string)
+}
