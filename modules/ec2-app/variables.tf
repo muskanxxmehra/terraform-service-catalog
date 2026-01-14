d1ab6e3b6d80ad16b4a5962bf7ce0d7819974722
muskanxxmehra <muskan.mehra@azalio.io>
2026-01-15 02:10:12 +0530

    Update variables.tf

--- modules/ec2-app/variables.tf
+++ modules/ec2-app/variables.tf
@@ -1,7 +1,66 @@
-variable "ami_id" {}
-variable "instance_type" {}
-variable "subnet_id" {}
-variable "security_group_id" {}
-variable "app_name" {}
-variable "user_data" {}
+################################################################################
+# EC2 App Module - Variables
+################################################################################
 
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type (t2.micro for Free Tier)"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where the instance will be launched"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "Security group ID to attach to the instance"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Name of the SSH key pair"
+  type        = string
+  default     = null
+}
+
+variable "app_name" {
+  description = "Name of the application"
+  type        = string
+  default     = "web-app"
+}
+
+variable "environment" {
+  description = "Environment name for tagging"
+  type        = string
+  default     = "dev"
+}
+
+variable "root_volume_size" {
+  description = "Size of the root EBS volume in GB (Free Tier: up to 30GB)"
+  type        = number
+  default     = 8
+}
+
+variable "user_data" {
+  description = "User data script for instance initialization"
+  type        = string
+  default     = ""
+}
+
+variable "create_eip" {
+  description = "Whether to create an Elastic IP for the instance"
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default     = {}
+}
